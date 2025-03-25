7565c35c5d17a4fd57c0d17fc81c57a0f42be0d4
huyluong.dev <toilahuyluong@gmail.com>
2025-03-25 22:39:36 +0700

    add tab0

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # app.py
 
 import streamlit as st
-from pages import tab1_existing_project, tab2_custom_analysis
+from pages import tab1_existing_project, tab2_custom_analysis, tab0_readme
 
 st.set_page_config(page_title="Tokenomics Lab", layout="wide")
 
 st.title("🚀 Tokenomics Analysis & Evaluation")
 
-tab1, tab2 = st.tabs(["📊 Dự Án Có Sẵn", "🧪 Phân Tích Tùy Chỉnh"])
+tab0, tab1, tab2 = st.tabs(["📘Readme- Hướng dẫn sử dụng", "📊 Dự Án Có Sẵn", "🧪 Phân Tích Tùy Chỉnh"])
+
+with tab0:
+    tab0_readme.render_tab0()
 
 with tab1:
     tab1_existing_project.render_tab()
